d60396070cab1b9a7e38b1e5aca43f1c0fdcc3de
alexandranimhagen <nimhagen@hotmail.com>
2024-06-28 16:05:19 +0200

    Initial commit

--- src/main/java/com/example/gritacademyapi/controller/CourseController.java
+++ src/main/java/com/example/gritacademyapi/controller/CourseController.java
@@ -4,6 +4,7 @@ import com.example.gritacademyapi.model.Course;
 import com.example.gritacademyapi.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+git remote add origin https://github.com/alexandranimhagen/Java23-WebServices_GritAcademyAPI-alexandra-nimhagen.git
 
 import java.util.List;
 
